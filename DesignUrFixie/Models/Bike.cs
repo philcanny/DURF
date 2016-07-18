8ec81601a1518fc85b17581357353846fba00056
philcanny <pcanny@LT101546>
2016-07-18 23:24:51 +0100

    Create Page Modified, @media settings adjusted

--- DesignUrFixie/Models/Bike.cs
+++ DesignUrFixie/Models/Bike.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+//using System.ComponentModel.DataAnnotations;
 
 namespace DesignUrFixie.Models
 {
+    
     public class Bike
     {
         public int BikeId { get; set; }
+        //[Required]
         public string BikeName { get; set; }
+        //[Required]
         public string FrameColour { get; set; }
+        //[Required]
         public string SaddleColour { get; set; }
+        //[Required]
         public string HandlebarColour { get; set; }
+        //[Required]
         public string WheelColour { get; set; }
 
         internal static void SaveChanges()
